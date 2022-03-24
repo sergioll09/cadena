0a5cbe667562102315705fcac8bd198890935bb6
Sergio <sergioll09@educastur.es>
2022-03-24 13:49:42 +0100

    primerCommit

---
+++ cadena/src/cadena/Cadena.java
@@ -0,0 +1,22 @@
+package cadena;
+
+public class Cadena {
+
+	
+	
+	public int longitud(String cadena) {
+		return cadena;
+	}
+	
+	public int vocales(String cadena) {
+		return cadena;
+	}
+	
+	public String invertir(String cadena) {
+		return cadena;
+	}
+	
+	public int contarLetra(String cadena, char carácter) {
+		return cadena;
+	}
+}
